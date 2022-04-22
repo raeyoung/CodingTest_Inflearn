73be6007d05681b0e73043f1fa628265feb53ad7
raeyoung <raeyoung233@gmail.com>
2022-04-22 11:20:45 +0900

    섹션1_07 문제풀이 (회문문자열)

---
+++ CodingTest_Inflearn/src/main/java/Section1_07.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+// 회문문자열 (앞에 읽거나 뒤에서 읽을 떄 같은 문자열)
+public class Section1_07 {
+    public String solution(String str) {
+        // String answer = "YES";
+        // 1. toUpperCase 사용하여 문자열 확인
+        /*
+        str = str.toUpperCase();
+        int len = str.length();
+        for(int i=0 ; i<len/2 ; i++) {
+            if(str.charAt(i) != str.charAt(len-i-1)) {
+                return "NO";
+            }
+        }
+         */
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();   // 뒤집기
+        if(str.equalsIgnoreCase(tmp)) answer = "YES";
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Section1_07 s = new Section1_07();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(s.solution(str));
+    }
+}

f34a5247a6660be4ae349b57d1834cb408de31b4
raeyoung <raeyoung233@gmail.com>
2022-04-22 10:58:27 +0900

    섹션1_06 문제풀이 (중복문자 제거)

---
+++ CodingTest_Inflearn/src/main/java/Section1_06.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+// 중복문자 제거 (indexOf)
+public class Section1_06 {
+    public String solution(String str) {
+        String answer = "";
+
+        // indexOf 사용
+        for(int i = 0 ; i < str.length() ; i++) {
+            // str.charAt(i) : 해당 문자열 위치 , str.indexOf(str.charAt(i)) : 해당 문자열 처음 발견된 위치
+            // System.out.println(str.charAt(i) + " " + i + " " + str.indexOf(str.charAt(i)));
+            if(str.indexOf(str.charAt(i)) == i) {   // true 인 경우 중복 X
+                answer+= str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Section1_06 s = new Section1_06();
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(s.solution(str));
+    }
+}

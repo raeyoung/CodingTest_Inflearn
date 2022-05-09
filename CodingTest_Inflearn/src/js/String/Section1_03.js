5e8f0203315e4e69a8bc828bf0da30a7d570383e
raeyoung <raeyoung233@gmail.com>
2022-05-09 11:09:08 +0900

    자바스크립트 섹션1_03 문제풀이(문장 속 단어 찾기_indexOf(), subString() 사용)

--- CodingTest_Inflearn/src/js/String/Section1_03.js
+++ CodingTest_Inflearn/src/js/String/Section1_03.js
@@ -1,7 +1,9 @@
 // 문장 속 단어 찾기(가장 긴 단어 찾기)
-const str = 'it is time to study';
+let str = 'it is time to study';
 let answer = '';
 
+//1. split 사용
+/*
 let max = Number.MIN_SAFE_INTEGER;
 
 const arr = str.split(' ');
@@ -14,4 +16,20 @@ for(let i=0 ; i<arr.length ; i++) {
         answer = arr[i];
     }
 }
+console.log(answer);
+*/
+
+// 2. indexOf(), subString() 사용
+let max = Number.MIN_SAFE_INTEGER, pos;
+
+while((pos = str.indexOf(" ")) != -1) {
+    let tmp = str.substring(0, pos);
+    let len = tmp.length;
+    if(len > max) {
+        max = len;
+        answer = tmp;
+    }
+    str = str.substring(pos +1);
+}
+if(str.length > max) answer = str;
 console.log(answer);

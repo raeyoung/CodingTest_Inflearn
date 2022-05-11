37a06188b1bd24b1a0f8a5355397cf0ed8966624
raeyoung <raeyoung233@gmail.com>
2022-05-11 09:51:41 +0900

    자바스크립트 섹션1_06 문제풀이 (중복문자 제거)

---
+++ CodingTest_Inflearn/src/js/String/Section1_06.js
@@ -0,0 +1,11 @@
+// 중복문자 제거
+let str = 'ksekkset';
+let answer = '';
+
+for(let i=0 ; i<str.length ; i++) {
+    // 해당 문자열이 발견된 위치와 같다면 
+    if(str.indexOf(str[i]) == i) {
+        answer += str[i];
+    }
+}
+console.log(answer);

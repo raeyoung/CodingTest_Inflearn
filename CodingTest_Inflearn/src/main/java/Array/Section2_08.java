6b28879b35aba750cefc49fe53d8b0ab8a6d359e
raeyoung <raeyoung233@gmail.com>
2022-05-12 16:23:06 +0900

    섹션1_08 문제풀이 (등수 구하기)

---
+++ CodingTest_Inflearn/src/main/java/Array/Section2_08.java
@@ -0,0 +1,35 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 등수구하기
+public class Section2_08 {
+    public int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];  // 동적으로 선언하면 0으로 초기화
+
+        for(int i=0 ; i<n ; i++) {
+            int cnt =1;
+            for(int j=0 ; j<n ; j++) {
+                if(arr[j] > arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Section2_08 s = new Section2_08();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0 ; i<n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : s.solution(n, arr)) {
+            System.out.println(x + " ");
+        }
+    }
+}

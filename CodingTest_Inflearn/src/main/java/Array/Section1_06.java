8d777b086758d0324232a64c5e55981371126d38
raeyoung <raeyoung233@gmail.com>
2022-05-12 14:27:11 +0900

    섹션2_06 문제풀이 (뒤집은 소수)

---
+++ CodingTest_Inflearn/src/main/java/Array/Section1_06.java
@@ -0,0 +1,45 @@
+package Array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+// 뒤집은 소수
+public class Section1_06 {
+    // 소수 판별
+    public boolean isPrime(int num) {
+        if(num == 1) return false;
+        for(int i=2 ; i<num ; i++) {
+            if(num%1==0) return false;
+        }
+        return true;
+
+    }
+    public ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        for(int i=0 ; i<n ; i++) {
+            int tmp = arr[i];
+            int res = 0;
+            while(tmp > 0) {
+                int t = tmp % 10; // 나머지 구하기
+                res = res * 10 + t;
+                tmp = tmp / 10;
+            }
+            if(isPrime(res)) answer.add(res);
+
+        }
+        return answer;
+    }
+    //값 입력
+    public static void main(String[] args) {
+        Section1_06 s = new Section1_06();
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=0 ; i<n ; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for(int x : s.solution(n, arr)) {
+            System.out.println(x + " ");
+        }
+    }
+}
